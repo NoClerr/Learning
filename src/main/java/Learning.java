1a4360ba781d3995a6bafc72cf93a411532c5001
NoClerr <pozitiv071@gmail.com>
2025-06-12 00:39:05 +0300

    v0.1

--- src/main/java/Learning.java
+++ src/main/java/Learning.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Learning {
     public static void main(String[] args) {
-                        System.out.println("Yappy!");
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        for (int b = 1; b < a; b++) {
+            if (b % 3 == 0 && b % 5 == 0) {
+                System.out.println("FizzBuzz");
+            }
+            if (b % 3 == 0) {
+                System.out.println("Fizz");
+            }
+            if (b % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(b);
+            }
+        }
     }
 }
